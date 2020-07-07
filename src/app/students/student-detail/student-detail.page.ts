5706f38a5339343d828f0d43ea7b36ef4600d997
Fernando Wibowo <30991688+fwibowo88@users.noreply.github.com>
2020-07-07 07:51:52 +0700

    Load Data to Student Detail
    -Get Parameter in Student-detail.page.ts (Activated Route)
    -Create Variable to Wrap Loaded Data
    -Fill the Variable with Data
    -Load Data to View + Add Some Stylinh

--- src/app/students/student-detail/student-detail.page.ts
+++ src/app/students/student-detail/student-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StudentsService } from '../students.service';
+import { Student } from '../student.model';
 
 @Component({
   selector: 'app-student-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-detail.page.scss'],
 })
 export class StudentDetailPage implements OnInit {
+  loadedStudent:Student;
 
-  constructor() { }
+  constructor(
+    private activatedRoute:ActivatedRoute, 
+    private studentService:StudentsService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap=>{
+      if(!paramMap.has('studentID')){
+        //Redirect
+        return;
+      }
+      const studentID = paramMap.get('studentID');
+      this.loadedStudent = this.studentService.getStudent(studentID)
+    });
   }
 
 }
