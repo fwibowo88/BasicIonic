7b0f1d344a8694453336889c2e2fd6c3d4e8b4f4
Fernando Wibowo <30991688+fwibowo88@users.noreply.github.com>
2020-07-07 07:17:23 +0700

    Feeding Data to Students List
    -Create Student Model & Attribute
    -Create Student Object on Student Page.ts + Array
    -Load Data to View (Looping *ngFor & src Binding Image)

--- src/app/students/students.page.ts
+++ src/app/students/students.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from './student.model';
+import { sha1 } from '@angular/compiler/src/i18n/digest';
 
 @Component({
   selector: 'app-students',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.page.scss'],
 })
 export class StudentsPage implements OnInit {
-
+  students: Student[] = [
+    {
+      id:'s1',
+      name:'John Doe',
+      photo: 'https://i.pinimg.com/originals/35/4f/1a/354f1a5476d8a48a655611a847f726cf.png',
+      hobbies: ['Sport','Singing','Travelling']
+    },
+    {
+      id:'s2',
+      name:'Nobita Nobi',
+      photo: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/21c77039-c68a-4ad9-b6e3-fa31d39a7c4e/d8vtcbc-9f0e08ff-1ab4-43c0-b1f6-9d9347bf95da.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMjFjNzcwMzktYzY4YS00YWQ5LWI2ZTMtZmEzMWQzOWE3YzRlXC9kOHZ0Y2JjLTlmMGUwOGZmLTFhYjQtNDNjMC1iMWY2LTlkOTM0N2JmOTVkYS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.ksCJ1eIJO3pmR1NP0CXNMiEGCA7oE0NnJb9Qkd5Y01o',
+      hobbies: ['Travelling','Drawing','Eating']
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
